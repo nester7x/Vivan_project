715c4c7a96981952ea4374bb9ba8f1dc9c5bc5ca
Andrew <nester.letopisets@gmail.com>
2021-09-12 15:24:41 +0300

    header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 $(function () {
+    const menuArrow = document.querySelector('.menu__arrow');
 
+    menuArrow.onclick = function () {
+        menuArrow.parentElement.classList.toggle('active')
+    }
+
+    const burger = document.querySelector('.menu__btn');
+    const menu = document.querySelector('.menu');
+    const menuLink = document.querySelectorAll('.menu__link');
+    const menuSubLink = document.querySelectorAll('.menu__sub-link');
+    const body = document.querySelector('body');
+
+    burger.onclick = function () {
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+        body.classList.toggle('active');
+        menuArrow.parentElement.classList.remove('active')
+    };
+
+    menuLink.forEach(function (item) {
+        item.onclick = function () {
+            burger.classList.remove('active');
+            menu.classList.remove('active');
+            body.classList.remove('active');
+        };
+    })
+
+    menuSubLink.forEach(function (item) {
+        item.onclick = function () {
+            burger.classList.remove('active');
+            menu.classList.remove('active');
+            // body.classList.remove('active');
+            menuArrow.parentElement.classList.remove('active')
+        };
+    })
 })
